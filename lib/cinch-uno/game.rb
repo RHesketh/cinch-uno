3a829669564448b20027e0ec370d42a846b4dac7
Rob Hesketh <rhesketh.uk@gmail.com>
2016-09-27 13:21:12 +0100

    Added some more passing tests to begin the basic game formation outline.

--- lib/cinch-uno/game.rb
+++ lib/cinch-uno/game.rb
@@ -1,9 +1,31 @@
 module Uno
   class Game
     attr_reader :state
+    
+    def players
+      @players.keys
+    end
 
     def initialize
-      @state = :waiting_for_players
+      @state = :waiting_to_start
+      @players = {}
     end
+
+    def start
+      raise NotEnoughPlayers unless @players.keys.count >= 2 
+
+      @state = :waiting_for_player
+    end
+
+    def add_player(nickname)
+      #raise PlayerAlreadyInGame if @players[nickname]
+
+      @players[nickname] = {}
+
+    end
+
+    # Errors
+    class NotEnoughPlayers < StandardError; end
+    class PlayerAlreadyInGame < StandardError; end
   end
 end
--- spec/Uno/game_spec.rb
+++ spec/Uno/game_spec.rb
@@ -4,7 +4,47 @@ module Uno
   describe Game do
   let(:game) { Uno::Game.new }
     it 'Starts in a :waiting_for_players state' do
-      expect(game.state).to be :waiting_for_players
+      expect(game.state).to be :waiting_to_start
+    end
+
+    context "while waiting for players" do
+      it "don't start the game unless there are at least 2 players" do 
+        expect{
+          game.start
+        }.to raise_error(Game::NotEnoughPlayers)
+      end
+
+      it "multiple players can join the game" do 
+        game.add_player("Char")
+        game.add_player("angelphish")
+
+        expect(game.players).to be_an Array
+
+        expect(game.players[0]).to eq "Char"
+        expect(game.players[1]).to eq "angelphish"
+
+        expect(game.players.length).to eq 2
+      end
+
+      it "do nothing if a player tries to join twice" do 
+        game.add_player("Char")
+        game.add_player("Char")
+
+        expect(game.players.length).to eq 1
+      end
+    end
+
+    context "When the game is started with at least two players" do
+      before(:each) do 
+        game.add_player("Char")
+        game.add_player("angelphish")
+
+        game.start
+      end
+
+      it "should be in a :waiting_for_player state" do 
+        expect(game.state).to be :waiting_for_player
+      end
     end
   end
 end
