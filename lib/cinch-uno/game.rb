4e36c6b364e2389da5bd0220a277b3c2d0a34411
Rob Hesketh <rhesketh.uk@gmail.com>
2016-09-27 17:21:29 +0100

    Added tests to Uno::Game concerning dealing cards and taking turns.

--- lib/cinch-uno/game.rb
+++ lib/cinch-uno/game.rb
@@ -4,12 +4,10 @@ module Uno
     attr_reader :discard_pile
     attr_reader :draw_pile
     attr_reader :play_order
+    attr_reader :players
     
-    def players
-      @players.keys
-    end
 
-    def current_player
+    def current_player_name
       @play_order[@current_player]
     end
 
@@ -17,19 +15,21 @@ module Uno
       @state =          :waiting_to_start
 
 
-      @deck =           options[:deck]      || Uno::Deck.generate
-      @players =        options[:players]   || {}
+      @deck =               options[:deck]      || Uno::Deck.generate
+      @players =            options[:players]   || {}
     end
 
-    def start
-      raise NotEnoughPlayers unless @players.keys.count >= 2 
+    def start(options={})
+      raise NotEnoughPlayers unless @players.count >= 2 
 
-      @deck.shuffle!
+      @deck.shuffle! unless options[:shuffle_deck] == false
 
       @discard_pile = [@deck.pop]
       @draw_pile = @deck
-      @play_order = @players.keys.shuffle
+      @play_order = options[:static_play_order] == false ? @players.keys : @players.keys.shuffle
       @current_player = 0
+
+      deal_starting_cards_to_all_players if @players[current_player_name][:cards].nil?
 
       @state = :waiting_for_player
     end
@@ -38,8 +38,27 @@ module Uno
       @players[nickname] = {}
     end
 
+    def play(player_name, card_played)
+      raise GameHasNotStarted unless @state == :waiting_for_player
+      raise NotPlayersTurn unless current_player_name == player_name
+    end
+
     # Errors
+    class GameHasNotStarted < StandardError; end
     class NotEnoughPlayers < StandardError; end
     class PlayerAlreadyInGame < StandardError; end
+    class NotPlayersTurn < StandardError; end
+
+    private 
+
+    def deal_starting_cards_to_all_players
+      @players.keys.each do |player|
+        @players[player][:cards] = []
+
+        7.times do
+          @players[player][:cards] << @draw_pile.pop
+        end
+      end
+    end
   end
 end
--- spec/Uno/game_spec.rb
+++ spec/Uno/game_spec.rb
@@ -2,7 +2,14 @@ require 'spec_helper'
 
 module Uno
   describe Game do
-    let(:fake_deck) {[Uno::Card.new, Uno::Card.new, Uno::Card.new]}
+    let(:fake_deck) {
+      array = []
+      20.times do
+        array << Uno::Card.new
+      end
+
+      array
+    }
 
     let(:game) { Uno::Game.new(deck: fake_deck) }
 
@@ -10,7 +17,7 @@ module Uno
       expect(game.state).to be :waiting_to_start
     end
 
-    context "While waiting for players..." do
+    context "While waiting to start..." do
       it "Don't start the game unless there are at least 2 players" do 
         expect{
           game.start
@@ -21,10 +28,10 @@ module Uno
         game.add_player("Char")
         game.add_player("angelphish")
 
-        expect(game.players).to be_an Array
+        expect(game.players).to be_a Hash
 
-        expect(game.players[0]).to eq "Char"
-        expect(game.players[1]).to eq "angelphish"
+        expect(game.players.keys[0]).to eq "Char"
+        expect(game.players.keys[1]).to eq "angelphish"
 
         expect(game.players.length).to eq 2
       end
@@ -35,18 +42,19 @@ module Uno
 
         expect(game.players.length).to eq 1
       end
+
+      it "Moves are not accepted" do 
+        expect{
+          game.play("foo12", Uno::Card.new)
+        }.to raise_error(Game::GameHasNotStarted)
+      end
     end
 
     context "When the game is started with at least two players..." do
       let(:fake_players) { { "Char" => {}, "angelphish" => {} } }
       let(:game){ Uno::Game.new(players: fake_players)}
 
-      it "Begin in a :waiting_for_player state" do 
-        game.start
-        expect(game.state).to be :waiting_for_player
-      end
-
-      it "Generate own deck if not given one" do 
+      it "Generate a deck to play with" do 
         expect(Uno::Deck).to receive(:generate).and_call_original
 
         game.start
@@ -60,22 +68,31 @@ module Uno
         new_game.start
       end
 
+      it "Deals 7 cards to each player" do 
+        game.start
+        
+        game.players.each do |player, info|
+          expect(info[:cards]).to be_an Array
+          expect(info[:cards].length).to eq 7
+        end
+      end
+
       it "Place one card from the deck on the discard pile" do
         new_game = Uno::Game.new(deck: fake_deck, players: fake_players)
         new_game.start
 
         expect(new_game.discard_pile).to be_an Array
         expect(new_game.discard_pile.length).to eq 1
-        expect(new_game.discard_pile.first).to be_a Uno::Card
+        expect(new_game.discard_pile.last).to be_a Uno::Card
       end
 
-      it "Place two cards from the deck into a draw pile" do 
+      it "Place the remaining cards from the deck into a draw pile" do 
         new_game = Uno::Game.new(deck: fake_deck, players: fake_players)
         new_game.start
 
         expect(new_game.draw_pile).to be_an Array
-        expect(new_game.draw_pile.length).to eq 2
-        expect(new_game.draw_pile.first).to be_a Uno::Card
+        expect(new_game.draw_pile.length).to eq 5
+        expect(new_game.draw_pile.last).to be_a Uno::Card
       end
 
       it "Determine a play order" do 
@@ -83,9 +100,66 @@ module Uno
 
         expect(game.play_order).to be_an Array
         expect(game.play_order.length).to eq 2
+      end
 
-        expect(game.current_player).to be_a String
-        expect(game.current_player).to eq game.play_order.first
+      it "Expose the current player's name" do 
+        game.start
+
+        expect(game.current_player_name).to be_a String
+        expect(fake_players.keys.include?(game.current_player_name)).to be true
+      end
+
+      it "Begin in a :waiting_for_player state" do 
+        game.start
+        expect(game.state).to be :waiting_for_player
+      end
+    end
+
+    context "When a player gives their move..." do
+      let(:fake_hand) {[Uno::Card.new(color: :red, type: :one), Uno::Card.new(color: :red, type: :zero)]}
+      let(:fake_info) {{:cards => fake_hand}}
+      let(:fake_players) { { "Char" => fake_info, "angelphish" => fake_info } }
+      let(:game){ Uno::Game.new(players: fake_players)}
+
+      before(:each) do 
+        game.start(static_play_order: false, shuffle_deck: false)
+      end
+
+      it "Moves are only accepted from players who are in the game" do 
+        expect{
+          game.play("foo12", Uno::Card.new)
+        }.to raise_error(Game::NotPlayersTurn)
+      end
+
+      it "A player cannot move unless it is their turn" do
+        expect(game.current_player_name).to eq "Char"
+
+        expect{
+          game.play("angelphish", Uno::Card.new)
+        }.to raise_error(Game::NotPlayersTurn)
+      end
+    end
+
+    context "When a valid move is made..." do 
+      context "If that was the player's last card" do 
+        xit "The player wins the game"
+        xit "The state should be :game_over"
+      end
+
+      context "If the player still has cards in their hand" do
+       xit "The card that was just played is now on top of the discard pile"
+       xit "Gameplay moves to the next player in the list"
+       xit "The state should still be :waiting_for_player"
+      end
+    end
+
+    context "Valid moves..." do
+      xit "Match the color on top of the discard pile"
+      xit "Match the type on top of the discard pile"
+    end
+
+    context "Invalid moves..." do
+      xit "Do not match the color or the type" do 
       end
     end
   end
